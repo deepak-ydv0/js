300f96bae0297f061645eda5b68ecdd4086f928a
DPK-YDV <112889421+DPK-YDV@users.noreply.github.com>
2023-12-07 09:14:50 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,45 @@
+// Primitive
+
+// 7 type : String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score = 100;
+const scoreValue =100.3
+const isLoggedIn = false;
+const temperature = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id ===anotherId);
+
+const bigNumber = 89473277547594379882n;
+
+
+
+
+
+
+
+//************************************* */
+
+
+
+// Reference (Non primitive)
+// Array ,Objects,Functions 
+
+
+const heros = ["shaktiman","nagraj","doga"];
+
+
+let myObj = {
+    name:"Deepak",
+    age:19,
+}
+
+
+
+const myFunction = function(){
+    console.log("myFunction");
+}
+
+console.log(typeof bigNumber);

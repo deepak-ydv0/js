b5c7de9494aca612896f23bbbbe96b82d38355dc
DPK-YDV <112889421+DPK-YDV@users.noreply.github.com>
2023-12-07 05:32:19 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+// "use strict"    treate all js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+console.log(3 + 3);
+
+
+let name = "deepak"
+let age = 19;
+let isLoggedIn = true;
+
+
+//number => 2to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => value not assign
+//symbol => unique
+
+
+//object
+
+console.log(typeof "deepak");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);

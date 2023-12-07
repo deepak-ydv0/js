09d89b476b406f1e35f562756dd0b64e489f9791
DPK-YDV <112889421+DPK-YDV@users.noreply.github.com>
2023-12-07 10:46:41 +0000

    math and num

---
+++ 01_basics/05_string.js
@@ -0,0 +1,39 @@
+// const name = "Deepak";
+// const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const myName = new Object("Deepak-yadav-ji")
+// console.log(myName[0]);
+// console.log(myName.__proto__);
+
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName.charAt(3));
+// console.log(myName.indexOf("p"));
+
+// const newString = myName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = myName.slice(0, 5)
+// console.log(anotherString);
+
+
+
+// const newStr1 = "   Deepak    ";
+// console.log(newStr1);
+// console.log(newStr1.trim());
+
+
+
+
+const url = "https://deepak.com/deepak%20yadav"
+
+console.log(url.replace("%20","-"));
+console.log(url.includes('deepak'));
+
+
+console.log(myName.split('-'));
+
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,51 @@
+const score = 100
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum = 123.45897
+// console.log(otherNum.toPrecision(3));
+
+
+
+const nums = 1000000
+// console.log(nums.toLocaleString('en-IN'));
+
+
+
+
+
+
+
+/************************************** */
+
+
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(4.4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.5));
+
+// console.log(Math.sqrt(36));
+// console.log(Math.min(4,3,6,7,10));
+// console.log(Math.max(4,3,6,7,10));
+
+
+
+// console.log(Math.random()); // value between 0 to 1
+// console.log(Math.random()*10); //value between 0 to 10
+// console.log(Math.random()*10 + 1); //value between 1 to 10
+// console.log(Math.floor(Math.random()*10 + 1)); //value between 1 to 10
+
+
+const min = 10;
+const max =61;
+
+console.log(Math.floor(Math.random()*(max-min + 1) + min) );

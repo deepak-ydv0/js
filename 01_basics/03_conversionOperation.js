875adb47c0ce021d6220118c36b56cabdb16f203
DPK-YDV <112889421+DPK-YDV@users.noreply.github.com>
2023-12-07 05:50:01 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = "33ass";
+let run = null
+let marks = undefined;
+let isBoy = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// let valuesInNumber = Number(run)
+// console.log(typeof(valuesInNumber));
+// console.log(valuesInNumber);
+
+// let marksInNumber = Number(marks)
+// console.log(typeof(marksInNumber));
+// console.log(marksInNumber);
+
+// let boyInNumber = Number(isBoy)
+// console.log(typeof(boyInNumber));
+// console.log(boyInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+
+// let isLoggedIn =1
+// let isLoggedIn =""
+let isLoggedIn ="Deepak"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Deepak" => true
+
+
+
+let someNum = 33
+let stringNum = Number(someNum)
+console.log(typeof(stringNum));

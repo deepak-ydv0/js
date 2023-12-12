8ba0162913c17ab64080e3fca9d820bbb549fc14
DPK-YDV <kd806040@gmail.com>
2023-12-12 12:25:47 +0530

    project

---
+++ 09_project/script.js
@@ -0,0 +1,27 @@
+// generate a random color
+
+const randomColor = function (){
+    const hex = '0123456789ABCDEF'
+    let color = "#"
+    for(i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+let intervalId;
+const startChangingColor = function (){
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+    if (!intervalId) {
+        intervalId = setInterval(changeBgColor,1000)
+    }
+}
+const stopChangingColor = function (){
+    clearInterval(intervalId)
+    intervalId = null
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor)
+document.querySelector('#stop').addEventListener('click', stopChangingColor)
